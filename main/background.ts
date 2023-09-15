92fcc875ce5358a13ab905f003e7a437322275e2
yafox3 <ruslan.kret@yandex.ru>
2023-09-15 19:29:56 +0300

    init

---
+++ main/background.ts
@@ -0,0 +1,39 @@
+import { app } from 'electron';
+import serve from 'electron-serve';
+import { createWindow } from './helpers';
+
+const isProd: boolean = process.env.NODE_ENV === 'production';
+
+if (isProd) {
+  serve({ directory: 'app' });
+} else {
+  app.setPath('userData', `${app.getPath('userData')} (development)`);
+}
+
+(async () => {
+  await app.whenReady();
+
+  const mainWindow = createWindow('main', {
+    width: 800,
+    height: 600,
+    resizable: false,
+    autoHideMenuBar: true,
+    opacity: .97,
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  });
+
+  if (isProd) {
+    await mainWindow.loadURL('app://./home.html');
+    mainWindow.removeMenu()
+  } else {
+    const port = process.argv[2];
+    await mainWindow.loadURL(`http://localhost:${port}/home`);
+    mainWindow.webContents.openDevTools();
+  }
+})();
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
